7096697914f37adfff83fcbd02d5a0c2d5705d4a
Phuong221203 <hnguyenphuong09@gmail.com>
2025-10-05 16:34:50 +0700

    add lombok annotation

--- content-service/src/main/java/com/english/content_service/dto/request/ListeningRequest.java
+++ content-service/src/main/java/com/english/content_service/dto/request/ListeningRequest.java
@@ -4,9 +4,17 @@ import com.english.content_service.entity.ListeningTopic;
 import jakarta.persistence.Column;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ListeningRequest {
     private String name;
     private String transcript;
